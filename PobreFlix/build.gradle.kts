764b3c05dd164ad9f4ca264f845635e1c2836efa
dankkj <mailatorresneh@gmail.com>
2026-07-10 21:20:05 -0300

    Include PobreFlix module in build.gradle.kts

--- PobreFlix/build.gradle.kts
+++ PobreFlix/build.gradle.kts
@@ -2,3 +2,4 @@ import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 
 // Diz ao Gradle para aplicar as configurações globais neste plugin
 val cloudstream = extensions.getByName<CloudstreamExtension>("cloudstream")
+include(":PobreFlix")
