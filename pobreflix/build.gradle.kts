ad1139d4ebba2387d528b7abeb6c6a5198fe557e
dankkj <mailatorresneh@gmail.com>
2026-07-10 21:42:08 -0300

    Remove comment from build.gradle.kts
    Removed comment about applying global settings in Gradle.

--- pobreflix/build.gradle.kts
+++ pobreflix/build.gradle.kts
@@ -1,4 +1,2 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-
-// Diz ao Gradle para aplicar as configurações globais neste plugin
 val cloudstream = extensions.getByName<CloudstreamExtension>("cloudstream")
