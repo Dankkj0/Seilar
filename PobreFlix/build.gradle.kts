993c66cc110fecd43d2a2f21567d48d2db91906f
dankkj <mailatorresneh@gmail.com>
2026-07-10 21:23:13 -0300

    Remove project inclusion from build.gradle.kts

--- PobreFlix/build.gradle.kts
+++ PobreFlix/build.gradle.kts
@@ -2,4 +2,3 @@ import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 
 // Diz ao Gradle para aplicar as configurações globais neste plugin
 val cloudstream = extensions.getByName<CloudstreamExtension>("cloudstream")
-include(":PobreFlix")
